806ba6544ac6185a7e42dade5b927195e6e0ca69
Aaron Haughton <dreamcast2000@outlook.com>
2019-10-30 23:20:10 -0400

    added swift files for controller and view

--- Albums/Controllers/DetailAddViewController.swift
+++ Albums/Controllers/DetailAddViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  DetailAddViewController.swift
 //  Albums
 //
 //  Created by Joe Thunder on 10/30/19.
@@ -8,13 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class DetailAddViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
+    
 
 
 }
-
